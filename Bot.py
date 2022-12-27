e22bf8d441be9ea04650bb2ba693001f50d0c534
osuminer <connorstaschiak@gmail.com>
2022-12-26 22:01:08 -0500

    Setup framework for discord bot
    Co-authored-by: cswimkutto <cswimkutto@users.noreply.github.com>

--- Bot.py
+++ Bot.py
@@ -1,2 +1,27 @@
 import pydest
 import discord
+
+with open('.env', 'r') as f:
+	DISCORD_API_KEY = f.readline().rstrip('\n')
+	BUNGIE_API_KEY = f.readline().rstrip('\n')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+	print(f'{client.user} has connected')
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	if message.content.startswith("$test"):
+		print('test')
+		await message.channel.send("test")
+
+
+client.run(DISCORD_API_KEY)
