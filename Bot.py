12402f3f62c0cf8d342b9d7190606170155f1e79
osuminer <connorstaschiak@gmail.com>
2022-12-28 00:00:27 -0500

    Kind of working

--- Bot.py
+++ Bot.py
@@ -4,6 +4,7 @@ from discord import app_commands
 from discord.ext import tasks, commands
 import pprint
 import asyncio
+import requests
 
 # Set api key values from file
 with open('.env', 'r') as f:
@@ -15,7 +16,7 @@ bot = commands.Bot(command_prefix="$", intents=discord.Intents.default())
 
 # Setup pydest api wrapper
 destiny = pydest.Pydest(BUNGIE_API_KEY)
-
+HEADERS = {"X-API-Key":BUNGIE_API_KEY}
 
 # Sync commands with Discord on bot startup
 @bot.event
@@ -32,18 +33,22 @@ async def hello(interaction: discord.Interaction):
 	await interaction.response.send_message(f'Hey {interaction.user.mention}!')
 
 # @tasks.loop(count=1)
-async def SearchId(bungie_id: str):
-	player_details = await destiny.api.search_destiny_player(1, bungie_id)
-	memId = player_details['Response'][0]['membershipId']
-	print(memId)
-	return memId
+# async def SearchId(bungie_id: str):
+# 	player_details = await destiny.api.search_destiny_player(1, bungie_id)
+# 	memId = player_details['Response'][0]['membershipId']
+# 	print(memId)
+# 	return memId
 
-memId = int()
+# memId = int()
 
 @bot.tree.command(name="getplayer")
 @app_commands.describe(bungie_id = "Your bungie id")
 async def GetPlayer(interaction: discord.Interaction, bungie_id: str):
-	bot.loop.create_task(SearchId(bungie_id))
+	# bot.loop.create_task(SearchId(bungie_id))
+	bungie_id = bungie_id.replace('#', '%23')
+	url = f"https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/{1}/{bungie_id}/"
+	memId = requests.get(url, headers=HEADERS).json()['Response'][0]['membershipId']
+
 	await interaction.response.send_message(f"{interaction.user.name}, your membership id is {memId}", ephemeral=True)
 
 
