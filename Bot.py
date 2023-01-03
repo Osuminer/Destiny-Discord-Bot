2fecbedf88d65c49afc9c2590a2ad356ab320c13
osuminer <connorstaschiak@gmail.com>
2023-01-02 22:52:59 -0500

    nightfall command works

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 from keys import DISCORD_API_TOKEN, BUNGIE_API_TOKEN
-from PyDest import pydest
+import PyDest
 import discord
 from discord import app_commands
 from discord.ext import tasks, commands
@@ -9,7 +9,7 @@ import pprint
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.default())
 
 # Setup pydest api wrapper
-destiny = pydest(BUNGIE_API_TOKEN)
+destiny = PyDest.PyDest(BUNGIE_API_TOKEN)
 HEADERS = {"X-API-Key":BUNGIE_API_TOKEN}
 
 # Sync commands with Discord on bot startup
@@ -19,7 +19,7 @@ async def on_ready():
 
 	try:
 		synced = await bot.tree.sync()
-		print(f"Synced {len(synced)} commands")
+		print(f"Synced {len(synced)} commands\n")
 	except Exception as e:
 		print(e)
 
@@ -28,13 +28,39 @@ async def hello(interaction: discord.Interaction):
 	await interaction.response.send_message(f'Hey {interaction.user.mention}!')
 
 
-@bot.tree.command(name="getplayer")
-@app_commands.describe(bungie_id = "Your bungie id")
+@bot.tree.command(name="getmemid")
+@app_commands.describe(bungie_id = "Enter your bungie id")
 async def GetPlayer(interaction: discord.Interaction, bungie_id: str):
 
-	memId = destiny.GetMembershipIdFromBungieId(bungie_id, 1)
+	player = destiny.api.search_destiny_player(1, bungie_id)
+	memId = player['Response'][0]['membershipId']
 
+	print(f"{interaction.user.name}, your membership id is {memId}")
 	await interaction.response.send_message(f"{interaction.user.name}, your membership id is {memId}", ephemeral=True)
 
+@bot.tree.command(name="nightfall")
+async def Nightfall(interaction: discord.Interaction):
+	memId = 4611686018430110693
+	characterId = 2305843009265615844
+	character = destiny.api.get_character(1, memId, characterId, [204])
+
+	for activity in character['Response']['activities']['data']['availableActivities']:
+		decoded = destiny.decode_hash(activity['activityHash'], 'DestinyActivityDefinition')
+
+		if 'Nightfall: Master' in decoded['displayProperties']['name']:
+			name = decoded['displayProperties']['description']
+
+			modifierList = list()
+
+			for modifiers in decoded['modifiers']:
+				modifierHash = modifiers['activityModifierHash']
+				y = destiny.decode_hash(modifierHash, 'DestinyActivityModifierDefinition')
+				# print(y['displayProperties']['name'])
+				# print(y['displayProperties']['description'])
+				modifierList.append(y['displayProperties']['name'])
+
+	retVal = ','.join(modifierList)
+
+	await interaction.response.send_message(retVal)
 
 bot.run(DISCORD_API_TOKEN)
--- test.py
+++ test.py
@@ -5,44 +5,54 @@ TOKEN = 'd5edb10339334ef6bed1889657d8465a'
 
 destiny = PyDest.PyDest(TOKEN)
 
-memId = destiny.api.GetMembershipIdFromBungieId("Kutto#9246")
+# memId = destiny.api.GetMembershipIdFromBungieId("Kutto#9246")
+
+player = destiny.api.search_destiny_player(1, "Kutto#9246")
+memId = player['Response'][0]['membershipId']
+
+print(memId)
 
 # pprint.pprint(memId)
 
-e = destiny.api.GetProfileInfo(memId, 1, [100])
-c = destiny.api.get_character(1, memId, '2305843009265615844', [204])
+# ['Response']['profile']['data']['characterIds'][0]
+
+# e = destiny.api.get_profile(1, memId, [100])
+
+# pprint.pprint(e)
+
+# c = destiny.api.get_character(1, memId, '2305843009265615844', [204])
 
 # pprint.pprint(destiny.decode_hash(2148295091, 'DestinyInventoryItemDefinition'))
 
 # pprint.pprint(c)
 
 # Print Current Nightfall
-for activity in c['Response']['activities']['data']['availableActivities']:
-    x = destiny.decode_hash(
-        activity['activityHash'], 'DestinyActivityDefinition')
-    pprint.pprint(x)
+# for activity in c['Response']['activities']['data']['availableActivities']:
+#     x = destiny.decode_hash(
+#         activity['activityHash'], 'DestinyActivityDefinition')
+#     pprint.pprint(x)
 
-    if 'Nightfall: Master' in x['displayProperties']['name']:
-        name = x['displayProperties']['description']
-        # print(name)
-        # pprint(x)
+#     if 'Nightfall: Master' in x['displayProperties']['name']:
+#         name = x['displayProperties']['description']
+#         # print(name)
+#         # pprint(x)
 
-        # for rewards in x['rewards'][0]['rewardItems']:
-        #     # p.pprint(rewards)
-        #     pprint(destiny.decode_hash(
-        #         rewards['itemHash'], 'DestinyInventoryItemDefinition'))
+#         # for rewards in x['rewards'][0]['rewardItems']:
+#         #     # p.pprint(rewards)
+#         #     pprint(destiny.decode_hash(
+#         #         rewards['itemHash'], 'DestinyInventoryItemDefinition'))
 
-        for modifiers in x['modifiers']:
-            modifierHash = modifiers['activityModifierHash']
-            y = destiny.decode_hash(
-                modifierHash, 'DestinyActivityModifierDefinition')
-            # p.pprint(y)
-            print(y['displayProperties']['name'])
-            print(y['displayProperties']['description'])
+#         for modifiers in x['modifiers']:
+#             modifierHash = modifiers['activityModifierHash']
+#             y = destiny.decode_hash(
+#                 modifierHash, 'DestinyActivityModifierDefinition')
+#             # p.pprint(y)
+#             print(y['displayProperties']['name'])
+#             print(y['displayProperties']['description'])
 
-        # Print seasons of profile
-        for seasonHash in e['Response']['profile']['data']['seasonHashes']:
-            print(seasonHash)
-            x = destiny.decode_hash(seasonHash, "DestinySeasonDefinition")
-            # p.pprint(x)
-            print(x['displayProperties']['name'])
+#         # Print seasons of profile
+#         for seasonHash in e['Response']['profile']['data']['seasonHashes']:
+#             print(seasonHash)
+#             x = destiny.decode_hash(seasonHash, "DestinySeasonDefinition")
+#             # p.pprint(x)
+#             print(x['displayProperties']['name'])
