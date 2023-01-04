ba2d668b0bf46ac0355167dec83369e4e9617508
osuminer <connorstaschiak@gmail.com>
2023-01-04 12:24:06 -0500

    finished nitghfall command

--- Bot.py
+++ Bot.py
@@ -6,82 +6,94 @@ from discord.ext import tasks, commands
 import pprint
 
 # Constants
-TOP_MODIFIERS = ['Shielded Foes', 'Champion Foes', 'Double Nightfall Drops', 'Ashes to Ashes', 'Lightning Crystals', 'Acute Arc Burn']
+TOP_MODIFIERS = ['Shielded Foes', 'Champion Foes', 'Double Nightfall Drops', 'Ashes to Ashes', 'Lightning Crystals',
+                 'Acute Arc Burn', 'Match Game', 'Champions: Mob', 'Attrition']
 
 # Setup discord bot
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.default())
 
 # Setup pydest api wrapper
 destiny = PyDest.PyDest(BUNGIE_API_TOKEN)
-HEADERS = {"X-API-Key":BUNGIE_API_TOKEN}
+HEADERS = {"X-API-Key": BUNGIE_API_TOKEN}
 
 # Sync commands with Discord on bot startup
+
+
 @bot.event
 async def on_ready():
-	print(f'{bot.user} has connected')
+    print(f'{bot.user} has connected')
 
-	try:
-		synced = await bot.tree.sync()
-		print(f"Synced {len(synced)} commands\n")
-	except Exception as e:
-		print(e)
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands\n")
+    except Exception as e:
+        print(e)
+
 
 @bot.tree.command(name="hello")
 async def hello(interaction: discord.Interaction):
-	await interaction.response.send_message(f'Hey {interaction.user.mention}!')
+    await interaction.response.send_message(f'Hey {interaction.user.mention}!')
 
 
 # Print Membership Id
 @bot.tree.command(name="getmemid")
-@app_commands.describe(bungie_id = "Enter your bungie id")
+@app_commands.describe(bungie_id="Enter your bungie id")
 async def GetPlayer(interaction: discord.Interaction, bungie_id: str):
 
-	player = destiny.api.search_destiny_player(1, bungie_id)
-	memId = player['Response'][0]['membershipId']
+    player = destiny.api.search_destiny_player(1, bungie_id)
+    memId = player['Response'][0]['membershipId']
 
-	print(f"{interaction.user.name}, your membership id is {memId}")
-	await interaction.response.send_message(f"{interaction.user.name}, your membership id is {memId}", ephemeral=True)
+    print(f"{interaction.user.name}, your membership id is {memId}")
+    await interaction.response.send_message(f"{interaction.user.name}, your membership id is {memId}", ephemeral=True)
 
 
 # Print Nightfall information
 @bot.tree.command(name="nightfall")
 async def Nightfall(interaction: discord.Interaction):
-	memId = 4611686018430110693
-	characterId = 2305843009265615844
-	character = destiny.api.get_character(1, memId, characterId, [204])
+    memId = 4611686018430110693  # My hardcoded membership Id
+    characterId = 2305843009265615844  # My hardcoded character Id
 
-	# Cycle through and decode the activities
-	for activity in character['Response']['activities']['data']['availableActivities']:
-		decoded = destiny.decode_hash(activity['activityHash'], 'DestinyActivityDefinition')
+    character = destiny.api.get_character(1, memId, characterId, [204])
 
-		# Find the Master Nightfall activity and decode it
-		if 'Nightfall: Master' in decoded['displayProperties']['name']:
-			name = decoded['displayProperties']['description']
-			imageURL = destiny.api.get_image_url(decoded['pgcrImage'])
-			thumbnailURL = destiny.api.get_image_url(decoded['displayProperties']['icon'])
-			description = decoded['selectionScreenDisplayProperties']['description']
+    # Cycle through and decode the activities
+    for activity in character['Response']['activities']['data']['availableActivities']:
+        decoded = destiny.decode_hash(
+            activity['activityHash'], 'DestinyActivityDefinition')
 
-			modifierList = list()
-			modifierDesc = list()
+        # Find the Master Nightfall activity and decode it
+        if 'Nightfall: Master' in decoded['displayProperties']['name']:
+            name = decoded['displayProperties']['description']
+            imageURL = destiny.api.get_image_url(decoded['pgcrImage'])
+            thumbnailURL = destiny.api.get_image_url(
+                decoded['displayProperties']['icon'])
+            description = decoded['selectionScreenDisplayProperties']['description']
 
-			# Decode the modifiers
-			for modifiers in decoded['modifiers']:
-				modifierHash = modifiers['activityModifierHash']
-				activityDecoded = destiny.decode_hash(modifierHash, 'DestinyActivityModifierDefinition')
+            modifierList = list()
+            modifierDesc = list()
 
-				activityName = activityDecoded['displayProperties']['name']
+            # Decode the modifiers
+            for modifiers in decoded['modifiers']:
+                modifierHash = modifiers['activityModifierHash']
+                activityDecoded = destiny.decode_hash(
+                    modifierHash, 'DestinyActivityModifierDefinition')
 
-				if (activityName != '') and (activityName in TOP_MODIFIERS) and (activityName not in modifierList): 
-					modifierList.append(activityDecoded['displayProperties']['name'])
-					modifierDesc.append(activityDecoded['displayProperties']['description'])
+                activityName = activityDecoded['displayProperties']['name']
 
-	embed = discord.Embed(title=name, description=description, color=0x1f8b4c)
-	embed.set_author(name="Nightfall: Master")
-	embed.set_image(url=imageURL)
-	embed.set_thumbnail(url=thumbnailURL)
-	for i in range(len(modifierList)):
-		embed.add_field(name=modifierList[i], value=modifierDesc[i])
+                # if (activityName in TOP_MODIFIERS) and (activityName not in modifierList):
+                if (activityName in TOP_MODIFIERS):
+                    modifierList.append(
+                        activityDecoded['displayProperties']['name'])
+                    modifierDesc.append(
+                        activityDecoded['displayProperties']['description'])
 
-	await interaction.response.send_message(embed=embed)
+    embed = discord.Embed(title=name, color=0x1f8b4c)
+    embed.set_author(name="Nightfall: Master")
+    embed.set_image(url=imageURL)
+    embed.set_thumbnail(url=thumbnailURL)
+    # embed.add_field(name="Modifiers", value=', '.join(modifierList))
+    for i in range(len(modifierList)):
+        embed.add_field(name=modifierList[i], value=modifierDesc[i])
+
+    await interaction.response.send_message(embed=embed)
 
 bot.run(DISCORD_API_TOKEN)
